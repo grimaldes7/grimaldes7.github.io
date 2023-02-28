0769eae454421b50d44b3b06adb196a59afd4952
Adolfo Grimaldes <ismaeladolfogrimaldes@gmail.com>
2023-02-28 11:14:03 -0300

    Repositorio para Ticamas

--- javascript/funcion.js
+++ javascript/funcion.js
@@ -1,3 +1,6 @@
 
 
-
+ function mUp(obj) {
+    obj.style.backgroundColor= "rgba(34, 1, 97, 0.363)";
+    obj.innerHTML="Mi nombre es Enrique Otero, soy técnico superior en desarrollo de aplicaciones informaticas, y durante mi formación he trabajado con distintos lenguajes de programacion. Me considero un profesional capacitado con el paquete office, ademas me manejo con soltura en photoshop y premier , ya que soy un aficionado a la edición de videos e imagenes.";
+}
